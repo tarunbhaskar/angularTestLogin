f5146a26a4ae42141e35237cfc0c539bc97d9d2e
Tarun <tarunbhaskar01@gmail.com>
2018-05-11 09:57:56 -0500

    first Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  rForm: FormGroup;
+  post:any;                     
+  userPassword:string = '';
+  userName:string = '';
+  titleAlert:string = 'Username is required';
+
+  constructor(private fb: FormBuilder) {
+
+    this.rForm = fb.group({
+      'userName': [null, Validators.required],
+      'userPassword': [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(15)])],
+      'validate' : ''
+    });
+
+  }
+
+  ngOnInit() {
+  }
+
+  addPost(post) {
+    this.userPassword = post.userPassword;
+    this.userName = post.userName;
+
+    console.log(this.userPassword);
+  }
+
 }
